57fbf9ac93e55ee06b5eee716fb6ef7cd57f1db9
Shareef Abdelhaleem <76070406+ShareefAbdelhaleem@users.noreply.github.com>
2022-01-08 13:51:55 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import numpy as np;
+from flask import Flask, request, jsonify, render_template;
+import pickle;
+
+app = Flask (__name__);
+#model = pickle.load (open ('model.pkl', 'rb'));
+
+@app.route ('/pred/<p1>/<p2>', methods=['GET'])
+def pred(p1="1", p2="2"):
+
+    name= request.args.get ("user");
+
+    returnObj = {
+        "program": "prediction",
+        "Params" : {
+            "p1" : p1,
+            "p2" : p2,
+        },
+        "user" : name 
+    }
+    return jsonify (returnObj);
+
+if __name__ == "__main__":
+    app.run (debug=True);
